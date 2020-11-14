a99fc87d8d20de6d26179b93cfa7a508ebfb849b
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-11-14 15:13:49 -0700

    testing out first nodejs app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var bodyParser = require('body-parser')
-var urlEncodedParser = bodyParser.urlencoded({extended: false})
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,7 +12,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('pages/calc_rates.ejs', (req, res) => {
+  .post('/calc_rates.ejs', (req, res) => {
     res.render('calc_rates.ejs', {weight: req.body.weight, mailType: req.body.mailType})
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
